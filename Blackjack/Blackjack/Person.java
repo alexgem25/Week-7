ee0e797782f90495e8cbe130c4a7f3d1d7e5be01
Alexandra Germaine Manalang <alexandra.g.manalang@student.uts.edu.au>
2023-09-27 00:46:37 +1000

    first commit

---
+++ Blackjack/Blackjack/Person.java
@@ -0,0 +1,18 @@
+
+public abstract class Person {
+    protected String name;
+    protected Hand hand;
+
+    public void drawCard(Deck deck) {
+        hand.add(deck.removeCard());
+        if (hand.busted())
+            System.out.println(name + " busts with " + hand + "!");
+    }
+
+    public abstract void haveTurn(Deck deck);
+    
+    @Override
+    public String toString() {
+        return name + " has " + hand;
+    }
+}
